619ae30b87593311165a7d5822e0dd32f476370a
horsunlu1275 <horsunlu1275@gmail.com>
2024-02-25 13:25:24 +0000

    checkbox eklendi django kullanılarak

--- convert_app/forms.py
+++ convert_app/forms.py
@@ -5,3 +5,11 @@ class uploaded_file_form(forms.ModelForm):
     class Meta:
         model=uploaded_file
         fields = ["file"]
+
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        file_name = file.name
+        file_extension = file_name.split('.')[-1]
+        self.cleaned_data['file_name'] = file_name
+        self.cleaned_data['file_extension'] = file_extension
+        return file
--- convert_app/views.py
+++ convert_app/views.py
@@ -4,12 +4,22 @@ from .forms import uploaded_file_form
 
 # Create your views here.
 def homePage(req):
-      
     if req.method == 'POST':
         form = uploaded_file_form(req.POST, req.FILES)
         if form.is_valid():
-            form.save()
-            return JsonResponse({'success': True})
+            form.clean_file()
+            uploaded_file_instance = form.save(commit=False)
+            uploaded_file_instance.save()
+            file_name = form.cleaned_data['file_name']
+            file_extension = form.cleaned_data['file_extension']
+            conversion_options = {
+                'txt': ['pdf', 'docx'],
+                'pdf': ['txt', 'docx'],
+                'docx': ['txt', 'pdf']
+                # Buraya istediğiniz diğer dönüştürme seçeneklerini ekleyebilirsiniz
+            }
+            options = conversion_options.get(file_extension, [])
+            return render(req, 'convert_app/homePage.html', {'form': form, 'file_name': file_name, 'file_extension': file_extension, 'conversion_options': options})
     else:
         form = uploaded_file_form()
     return render(req, 'convert_app/homePage.html', {'form': form})
